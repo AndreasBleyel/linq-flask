879a8a7524352fc1a87a7634f586d466412caf57
andy-b23 <andy-b23@gmx.at>
2019-12-19 16:00:36 +0100

    words added

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+words_set = []
+
+
+def read_word_file():
+    with open("words", "r") as f:
+        for item in f:
+            line = item.rstrip('\n')
+            words_set.append(line)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    read_word_file()
+    return 'Hello ' + str(len(words_set))
 
 
 if __name__ == '__main__':
