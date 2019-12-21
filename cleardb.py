1baf5cd7365afb5eca061d2ef698b47350d79a51
andy-b23 <andy-b23@gmx.at>
2019-12-21 14:02:57 +0100

    cleardb und styling

--- cleardb.py
+++ cleardb.py
@@ -6,5 +6,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 def reset_db():
     db.session.execute('''TRUNCATE TABLE games''')
     db.session.execute('''TRUNCATE TABLE cards''')
+    db.session.execute('''ALTER SEQUENCE games_id_seq RESTART WITH 1''')
+    db.session.execute('''ALTER SEQUENCE cards_id_seq RESTART WITH 1''')
+    db.session.commit()
 
 reset_db()
